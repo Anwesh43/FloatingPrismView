866bafb39b3bbdd2a556867827d84f04fc5543f9
Anwesh43 <anweshthecool0@gmail.com>
2016-11-11 15:38:55 +0530

    In the demo adding icons and position the View

--- app/src/main/java/com/anwesome/games/floatingprisview/MainActivity.java
+++ app/src/main/java/com/anwesome/games/floatingprisview/MainActivity.java
@@ -1,6 +1,7 @@
 package com.anwesome.games.floatingprisview;
 
 import android.content.DialogInterface;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,12 +18,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         PrisView prisView = (PrisView)findViewById(R.id.my_pris_view);
-        prisView.setX(200);
+        prisView.setX(100);
         prisView.setY(200);
         PrisButton button1 = PrisButton.newInstance();
         PrisButton button2 = PrisButton.newInstance();
         PrisButton button3 = PrisButton.newInstance();
         PrisButton button4 = PrisButton.newInstance();
+        button1.setIcon(BitmapFactory.decodeResource(getResources(),R.drawable.mail_icon));
+        button2.setIcon(BitmapFactory.decodeResource(getResources(),R.drawable.phone_white));
+        button3.setIcon(BitmapFactory.decodeResource(getResources(),R.drawable.cart_white));
+        button4.setIcon(BitmapFactory.decodeResource(getResources(),R.drawable.avtaar_white_icon));
         setButtonsAction(button1,button2,button3,button4);
         prisView.addButton(button1,button2,button3,button4);
 
